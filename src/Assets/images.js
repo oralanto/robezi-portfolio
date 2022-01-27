ef5ed9daf1722e61529051791130b5407724ea47
Osee-Ralantoarison <o.ralanto@gmail.com>
2022-01-27 09:17:17 +0100

    setup img

---
+++ src/Assets/images.js
@@ -0,0 +1,53 @@
+import image1 from "./images/image1.png";
+import image2 from "./images/image2.png";
+import image3 from "./images/image3.png";
+import image4 from "./images/image4.png";
+import image5 from "./images/image5.png";
+import image6 from "./images/image6.png";
+import image7 from "./images/image7.png";
+import image8 from "./images/image8.png";
+
+const images = [
+  {
+    title: "image",
+    description: "Une image",
+    img: image1,
+  },
+  {
+    title: "image",
+    description: "Une image",
+    img: image2,
+  },
+  {
+    title: "image",
+    description: "Une image",
+    img: image3,
+  },
+  {
+    title: "image",
+    description: "Une image",
+    img: image4,
+  },
+  {
+    title: "image",
+    description: "Une image",
+    img: image5,
+  },
+  {
+    title: "image",
+    description: "Une image",
+    img: image6,
+  },
+  {
+    title: "image",
+    description: "Une image",
+    img: image7,
+  },
+  {
+    title: "image",
+    description: "Une image",
+    img: image8,
+  },
+];
+
+export default images;
